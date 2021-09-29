14acc64ec03ca54df3a803341b7034aa7aca2819
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-09-29 17:29:56 -0300

    Created migration files

---
+++ database/migrations/000001_init_schema.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(80) NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    -- Define CONSTRAINTS
+    CONSTRAINT users_id_pk PRIMARY KEY (id),
+    CONSTRAINT users_username_uk UNIQUE (username),
+    CONSTRAINT users_email_uk UNIQUE (email),
+);

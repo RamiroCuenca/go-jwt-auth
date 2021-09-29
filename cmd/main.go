eb53f8015607eb70a648c11e11bc16ba7bc49381
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-09-29 16:35:13 -0300

    Downloaded lib/pq and created the connection to database package

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/RamiroCuenca/go-jwt-auth/common/logger"
+import (
+	"github.com/RamiroCuenca/go-jwt-auth/common/logger"
+	"github.com/RamiroCuenca/go-jwt-auth/database/connection"
+)
 
 func main() {
 	// Init zap logger
 	logger.InitZapLogger()
+
+	// Init postgre database
+	connection.NewPostgresClient()
 }
